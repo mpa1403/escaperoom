7c4ec39e62f16ad50be8cd6bee8f42ec803e5dd4
mpa1403 <mpa1403@github.com>
2020-06-20 14:54:07 -0400

    added some birthday messages

--- overlay.js
+++ overlay.js
@@ -10,12 +10,14 @@ export default class Overlay {
 
         let text;
         if (this.type == overlays.START) {
-            text = "You find yourself in a room\n\nthat looks like it was made\n\nin the Mac version of\n\nMicrosoft Paint.\n\nCan you escape?"
+            text = "You find yourself in a room\n\nthat looks like it was made\n\nin the Mac version of\n\nMicrosoft Paint.\n\nCan you escape?";
             this.startButton = this.scene.add.image(-1000, -1000, 'startButton').setName(identifiers.STARTBUTTON);
             this.startButton.setInteractive(Phaser.Geom.Rectangle());
             this.startButton.setDepth(2000);
         } else {
-            text = "You escaped the room!!!\n\nGreat job!\n\n(Sy was too lazy to implement\n\na 'Play Again' button\n\nso refresh the page if you\n\nwant another go at it)"
+            text = "You escaped the room!!!\n\nGreat job!\n\n(Sy was too lazy to implement\n\na 'Play Again' button\n\nso refresh the page if you\n\nwant another go at it)";
+            this.bottomText = this.scene.add.text(-1000, -1000, "Also happy birthday Len :)", { color: '#fff', fontSize: 20, align: 'center' });
+            this.bottomText.setDepth(2000);
         }
 
         this.text = this.scene.add.text(-1000, -1000, text, { color: '#fff', fontSize: 38, align: 'center' });
@@ -29,6 +31,7 @@ export default class Overlay {
             this.startButton.setPosition(400, 500);
         } else {
             this.text.setPosition(50, 50);
+            this.bottomText.setPosition(250, 540);
         }
     }
 
